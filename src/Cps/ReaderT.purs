280ce1db9b96d5a8d020cfd43d3887e91e76fccf
PureFunctor <purefunctor@gmail.com>
2022-05-16 11:44:27 +0800

    Implement ReaderT and WriterT

---
+++ src/Cps/ReaderT.purs
@@ -0,0 +1,125 @@
+module Cps.ReaderT where
+
+import Prelude
+
+import Control.Lazy (class Lazy)
+import Control.Monad.Error.Class (class MonadError, class MonadThrow, catchError, throwError)
+import Control.Monad.Reader.Class (class MonadAsk, class MonadReader)
+import Control.Monad.Rec.Class (class MonadRec)
+import Control.Monad.State.Class (class MonadState, state)
+import Control.Monad.Trans.Class (class MonadTrans, lift)
+import Control.Monad.Writer.Class (class MonadTell, class MonadWriter, listen, pass, tell)
+import Cps.RWSET (RWSET(..), hoistRWSET, runRWSET)
+import Data.Either (Either(..))
+import Data.Function.Uncurried (mkFn6, runFn3, runFn6)
+import Data.Tuple.Nested ((/\))
+import Effect.Class (class MonadEffect)
+
+newtype ReaderT r m a = ReaderT (RWSET r Unit Unit Void m a)
+
+runReaderT :: forall r m a. MonadRec m => r -> ReaderT r m a -> m a
+runReaderT r (ReaderT k) = go <$> runRWSET r unit k
+  where
+  go (_ /\ ea /\ _) =
+    case ea of
+      Left e ->
+        absurd e
+      Right a ->
+        a
+
+hoistReaderT :: forall r m n a. (m ~> n) -> ReaderT r m a -> ReaderT r n a
+hoistReaderT f (ReaderT k) = ReaderT (hoistRWSET f k)
+
+mapReaderT
+  :: forall r m1 m2 a1 a2
+   . MonadRec m1
+  => Functor m2
+  => (m1 a1 -> m2 a2)
+  -> ReaderT r m1 a1
+  -> ReaderT r m2 a2
+mapReaderT f k = ReaderT
+  ( RWSET
+      ( mkFn6 \environment _ _ lift' _ done ->
+          lift' $ f (runReaderT environment k) <#> \a _ ->
+            runFn3 done unit a unit
+      )
+  )
+
+withReaderT
+  :: forall r m a
+   . (r -> r)
+  -> ReaderT r m a
+  -> ReaderT r m a
+withReaderT f (ReaderT (RWSET k)) = ReaderT
+  ( RWSET
+      ( mkFn6 \environment state more lift' error done ->
+          runFn6 k (f environment) state more lift' error done
+      )
+  )
+
+derive newtype instance Functor (ReaderT r m)
+derive newtype instance Apply (ReaderT r m)
+derive newtype instance Applicative (ReaderT r m)
+derive newtype instance Bind (ReaderT r m)
+derive newtype instance Monad (ReaderT r m)
+derive newtype instance MonadEffect m => MonadEffect (ReaderT r m)
+derive newtype instance MonadRec (ReaderT r m)
+derive newtype instance MonadAsk r (ReaderT r m)
+derive newtype instance MonadReader r (ReaderT r m)
+derive newtype instance MonadTrans (ReaderT r)
+derive newtype instance Semigroup a => Semigroup (ReaderT r m a)
+derive newtype instance Monoid a => Monoid (ReaderT r m a)
+derive newtype instance Lazy (ReaderT r m a)
+
+instance (Monoid w, MonadTell w m) => MonadTell w (ReaderT r m) where
+  tell = lift <<< tell
+
+instance (Monoid w, MonadRec m, MonadWriter w m) => MonadWriter w (ReaderT r m) where
+  listen (ReaderT k) = ReaderT
+    ( RWSET
+        ( mkFn6 \environment state _ lift' error done -> do
+            lift' $ (listen $ runRWSET environment state k) <#> \((s /\ ea /\ _) /\ w) _ ->
+              case ea of
+                Left e ->
+                  runFn3 error s e unit
+                Right a ->
+                  runFn3 done s (a /\ w) unit
+        )
+    )
+
+  pass (ReaderT kf) = ReaderT
+    ( RWSET
+        ( mkFn6 \environment state _ lift' error done -> do
+            lift' $ pass $ runRWSET environment state kf <#> \(s /\ eaf /\ w) ->
+              case eaf of
+                Left e ->
+                  (\_ -> runFn3 error s e w) /\ identity
+                Right (a /\ f) ->
+                  (\_ -> runFn3 done s a w) /\ f
+        )
+    )
+
+instance MonadState s m => MonadState s (ReaderT r m) where
+  state = lift <<< state
+
+instance MonadThrow e m => MonadThrow e (ReaderT r m) where
+  throwError = lift <<< throwError
+
+instance (MonadRec m, MonadError e m) => MonadError e (ReaderT r m) where
+  catchError (ReaderT k) f = ReaderT
+    ( RWSET
+        ( mkFn6 \environment state _ lift' error done ->
+            lift' $
+              catchError (runRWSET environment state k)
+                ( \e ->
+                    case f e of
+                      ReaderT fk ->
+                        runRWSET environment state fk
+                ) <#> \(s /\ ea /\ w) _ ->
+                case ea of
+                  Left e ->
+                    runFn3 error s e w
+                  Right a ->
+                    runFn3 done s a w
+        )
+    )
---
+++ src/Cps/WriterT.purs
@@ -0,0 +1,110 @@
+module Cps.WriterT where
+
+import Prelude
+
+import Control.Lazy (class Lazy)
+import Control.Monad.Error.Class (class MonadError, class MonadThrow, catchError, throwError)
+import Control.Monad.Reader.Class (class MonadAsk, class MonadReader, ask, local)
+import Control.Monad.Rec.Class (class MonadRec)
+import Control.Monad.State.Class (class MonadState, state)
+import Control.Monad.Trans.Class (class MonadTrans, lift)
+import Control.Monad.Writer.Class (class MonadTell, class MonadWriter)
+import Cps.RWSET (RWSET(..), hoistRWSET, runRWSET)
+import Data.Either (Either(..))
+import Data.Function.Uncurried (mkFn6, runFn3)
+import Data.Tuple (fst, snd)
+import Data.Tuple.Nested (type (/\), (/\))
+import Effect.Class (class MonadEffect)
+
+newtype WriterT w m a = WriterT (RWSET Unit w Unit Void m a)
+
+runWriterT :: forall w m a. Monoid w => MonadRec m => WriterT w m a -> m (a /\ w)
+runWriterT (WriterT k) = go <$> runRWSET unit unit k
+  where
+  go (_ /\ ea /\ w) =
+    case ea of
+      Left e ->
+        absurd e
+      Right a ->
+        a /\ w
+
+evalWriterT :: forall w m a. Monoid w => MonadRec m => WriterT w m a -> m a
+evalWriterT k = fst <$> runWriterT k
+
+execWriterT :: forall w m a. Monoid w => MonadRec m => WriterT w m a -> m w
+execWriterT k = snd <$> runWriterT k
+
+hoistWriterT :: forall w m n a. (m ~> n) -> WriterT w m a -> WriterT w n a
+hoistWriterT f (WriterT k) = WriterT (hoistRWSET f k)
+
+mapWriterT
+  :: forall w1 w2 m1 m2 a1 a2
+   . Monoid w1
+  => Monoid w2
+  => MonadRec m1
+  => Functor m2
+  => (m1 (a1 /\ w1) -> m2 (a2 /\ w2))
+  -> WriterT w1 m1 a1
+  -> WriterT w2 m2 a2
+mapWriterT f k = WriterT
+  ( RWSET
+      ( mkFn6 \_ _ _ lift' _ done ->
+          lift' $ f (runWriterT k) <#> \(a /\ w) _ ->
+            runFn3 done unit a w
+      )
+  )
+
+derive newtype instance Monoid w => Functor (WriterT w m)
+derive newtype instance Monoid w => Apply (WriterT w m)
+derive newtype instance Monoid w => Applicative (WriterT w m)
+derive newtype instance Monoid w => Bind (WriterT w m)
+derive newtype instance Monoid w => Monad (WriterT w m)
+derive newtype instance (Monoid w, MonadEffect m) => MonadEffect (WriterT w m)
+derive newtype instance Monoid w => MonadRec (WriterT w m)
+derive newtype instance Monoid w => MonadTell w (WriterT w m)
+derive newtype instance Monoid w => MonadWriter w (WriterT w m)
+derive newtype instance Monoid w => MonadTrans (WriterT w)
+derive newtype instance (Monoid w, Semigroup a) => Semigroup (WriterT w m a)
+derive newtype instance (Monoid w, Monoid a) => Monoid (WriterT w m a)
+derive newtype instance Monoid w => Lazy (WriterT w m a)
+
+instance (Monoid w, MonadAsk r m) => MonadAsk r (WriterT w m) where
+  ask = lift ask
+
+instance (Monoid w, MonadRec m, MonadReader r m) => MonadReader r (WriterT w m) where
+  local f (WriterT k) = WriterT
+    ( RWSET
+        ( mkFn6 \environment state _ lift' error done ->
+            lift' $ local f (runRWSET environment state k) <#> \(s /\ ea /\ w) _ ->
+              case ea of
+                Left e ->
+                  runFn3 error s e w
+                Right a ->
+                  runFn3 done s a w
+        )
+    )
+
+instance (Monoid w, MonadState s m) => MonadState s (WriterT w m) where
+  state = lift <<< state
+
+instance (Monoid w, MonadThrow e m) => MonadThrow e (WriterT w m) where
+  throwError = lift <<< throwError
+
+instance (Monoid w, MonadRec m, MonadError e m) => MonadError e (WriterT w m) where
+  catchError (WriterT k) f = WriterT
+    ( RWSET
+        ( mkFn6 \environment state _ lift' error done ->
+            lift' $
+              catchError (runRWSET environment state k)
+                ( \e ->
+                    case f e of
+                      WriterT fk ->
+                        runRWSET environment state fk
+                ) <#> \(s /\ ea /\ w) _ ->
+                case ea of
+                  Left e ->
+                    runFn3 error s e w
+                  Right a ->
+                    runFn3 done s a w
+        )
+    )
